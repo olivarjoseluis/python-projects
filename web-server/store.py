fb7f564b7e7252db68d0234fcffc992672d92573
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-04-21 20:51:43 -0500

    Web-Server

---
+++ web-server/store.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def getCategories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json()
+    for category in categories:
+        print(category['name'])
